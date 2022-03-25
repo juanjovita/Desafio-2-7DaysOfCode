efa6ad9bfd74131b2dfa6d476dbff9b60851b76d
juanjovita <juanjovita22@gmail.com>
2022-03-25 00:36:41 -0300

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('hello!')
+
+
+
+
+
+function registrar() {
+var name = document.getElementById('nome').value
+var idade = document.getElementById('idade').value
+var linguagem = document.getElementById('linguagem').value
+var paragrafo1 = document.getElementById('resposta1')
+
+
+  resposta1 = 
+  ` <p>Olá ${name}! Você já tem ${idade} e está aprendendo ${linguagem}</p> <br>
+  <h3>Está gostando de estudar? Digite "1" para SIM e "2" para NÃO</h3> <br>`
+
+  paragrafo1.innerHTML = resposta1 + '<input type="number" id="opcao" min="1" max="2"> <button onclick="escolher()" id="button" type="button">REGISTRAR!</button>'
+}
+
+function escolher(){
+  var paragrafo2 = document.getElementById('resposta2')
+  var querestudar = document.getElementById('opcao').value
+
+  if(querestudar == 1){
+    resposta2 = 'Muito bom! Continue estudando e você terá muito sucesso!'
+  }
+  else{
+    resposta2 = 'Aahhh que pena... Já tentou aprender outras linguagens?'
+  }
+
+  paragrafo2.innerHTML = resposta2
+}
+
+console.log(registrar)
